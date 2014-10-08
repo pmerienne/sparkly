a81da56473e263f29103e9e21f75911622518f78
pmerienne <pierre.merienne@gmail.com>
2014-10-08 14:58:22 +0200

    Fix bootstrap-switch usage

--- src/main/webapp/directives/switch.js
+++ src/main/webapp/directives/switch.js
@@ -3,18 +3,11 @@ app.directive('switch', function() {
 	return {
 		restrict : "A",
 		link : function(scope, element, attrs) {
-			element.attr("data-on-color", "primary");
-			element.attr("data-off-color", "default");
-			element.attr("data-animate", "false");
-			// element.attr("data-on-text", "<i class='fa fa-check'></i>");
-			// element.attr("data-off-text", "<i class='fa fa-times'></i>");
-			element.attr("data-on-text", "ON");
-			element.attr("data-off-text", "OFF");
 			element.attr("checked", scope.property.value == "true");
-
 			$(element).bootstrapSwitch();
-			$(element).on('switchChange', function(e, data) {
-				scope.property.value = data.value.toString();
+
+			$(element).on('switchChange.bootstrapSwitch', function(e, data) {
+				scope.property.value = data.toString();
 			});
 		}
 	};
