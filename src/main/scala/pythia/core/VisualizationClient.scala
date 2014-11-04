99000d8673ca8cf3671c31fbaf5a116e505b524e
pmerienne <pierre.merienne@gmail.com>
2014-11-04 22:46:02 +0100

    Add latency visualization

--- src/main/scala/pythia/core/VisualizationClient.scala
+++ src/main/scala/pythia/core/VisualizationClient.scala
@@ -1,18 +1,18 @@
 package pythia.core
 
 import org.atmosphere.wasync.impl.{DefaultOptions, DefaultOptionsBuilder, DefaultRequestBuilder}
-import org.atmosphere.wasync.{Client, ClientFactory, Request}
+import org.atmosphere.wasync.{Socket, Client, ClientFactory, Request}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 import scala.concurrent.Future
 
-class VisualizationClient(val hostname: String, val port: Int, val master:String , val id: String) {
+class VisualizationClient(val hostname: String, val port: Int, val clusterId:String , val id: String) {
 
   import scala.concurrent.ExecutionContext.Implicits.global
   implicit val formats = DefaultFormats
 
-  val url = s"ws://${hostname}:${port}/api/visualization/${clean(master)}/${clean(id)}"
+  val url = s"ws://${hostname}:${port}/api/visualization/${clusterId}/${id}"
 
   def send(timestamp: Long, data: Map[String, Double]): Unit = send(VisualizationEvent(timestamp, data))
 
@@ -26,19 +26,30 @@ class VisualizationClient(val hostname: String, val port: Int, val master:String
   }
 
 
-  private def sendJson( json: String): Unit = {
-    val client: Client[DefaultOptions, DefaultOptionsBuilder, DefaultRequestBuilder] = ClientFactory.getDefault.newClient.asInstanceOf[Client[DefaultOptions, DefaultOptionsBuilder, DefaultRequestBuilder]]
-
-    val req = client.newRequestBuilder
-      .method(Request.METHOD.GET)
-      .uri(url)
-      .transport(Request.TRANSPORT.WEBSOCKET)
-
-    val opts = client.newOptionsBuilder().reconnect(false).build()
-    val socket = client.create(opts).open(req.build())
+  private def sendJson(json: String): Unit = {
+    val socket = VisualizationClient.getOrCreateSocket(url)
     socket.fire(json)
-    socket.close()
   }
 
-  private def clean(str: String) = str.replaceAll("[^a-zA-Z0-9/]" , "");
+}
+
+object VisualizationClient {
+  import scala.collection.mutable.Map
+
+  private val client: Client[DefaultOptions, DefaultOptionsBuilder, DefaultRequestBuilder] = ClientFactory.getDefault.newClient.asInstanceOf[Client[DefaultOptions, DefaultOptionsBuilder, DefaultRequestBuilder]]
+  private val opts = client.newOptionsBuilder().reconnect(false).build()
+
+  private val sockets: Map[String, Socket] = Map()
+  sys.addShutdownHook{ sockets.map(_._2).foreach(_.close()) }
+
+  protected def getOrCreateSocket(url: String): Socket = sockets.get(url) match {
+    case Some(socket) => socket
+    case None => {
+      val req = client.newRequestBuilder.method(Request.METHOD.GET).uri(url).transport(Request.TRANSPORT.WEBSOCKET)
+      val socket = client.create(opts).open(req.build())
+      sockets += url -> socket
+
+      socket
+    }
+  }
 }
---
+++ src/main/webapp/directives/viz/pipeline-latency.js
@@ -0,0 +1,70 @@
+app.directive('pipelineLatencyViz', function(VisualizationDataSource) {
+	return {
+		restrict : "E",
+		replace : true,
+		templateUrl : 'views/visualization/pipeline-latency.html',
+		scope : {
+			clusterId : "=cluster"
+		},
+		link : function(scope, element, attrs) {
+            scope.dataSource = new VisualizationDataSource(scope.clusterId, "pipeline-latency");
+            scope.dataSource.listen(function(event){
+                var time = event.timestampMs / 1000;
+                var data = {
+                    'Scheduling delay (ms)': event.data.schedulingDelay,
+                    'Processing delay (ms)': event.data.processingDelay
+                };
+                scope.graph.series.addData(data, time);
+                scope.graph.render();
+            });
+
+            scope.$on('$destroy', function() {
+                scope.dataSource.close();
+            });
+
+            scope.graph = new Rickshaw.Graph({
+                element: element.context.querySelector('.chart'),
+                renderer: 'area',
+                stroke: true,
+                height: 300,
+	            preserve: true,
+                series: new Rickshaw.Series.FixedDuration([{ name: 'Scheduling delay (ms)', color: 'steelblue' }, {name: 'Processing delay (ms)', color: 'lightblue'}], undefined, {
+                    timeInterval: 1000,
+                    maxDataPoints: 60
+                })
+            });
+
+            var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+                graph: scope.graph,
+                yFormatter: function(y) { return Math.floor(y) + " ms" },
+                xFormatter: function(x) {return new Date(x * 1000).toString();}
+            });
+
+            var legend = new Rickshaw.Graph.Legend( {
+                graph: scope.graph,
+                element: element.context.querySelector('.chart-legend')
+            });
+            var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+            	graph: scope.graph,
+            	legend: legend
+            } );
+
+            var xAxis = new Rickshaw.Graph.Axis.X( {
+                graph: scope.graph,
+                ticksTreatment: 'glow',
+                ticks: 5,
+                tickFormat: function(d) {return d3.time.format("%H:%M:%S")(new Date(d * 1000));}
+            } );
+            xAxis.render();
+
+            var yAxis = new Rickshaw.Graph.Axis.Y({
+                graph: scope.graph,
+                tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+                ticksTreatment: 'glow'
+            });
+            yAxis.render();
+
+            scope.graph.render();
+        }
+	};
+});
