def13bccc153532b4da58749f7a344471ac30784
pmerienne <pierre.merienne@gmail.com>
2014-11-09 17:06:28 +0100

    Add Memory viz

--- src/main/scala/pythia/core/VisualizationBuilder.scala
+++ src/main/scala/pythia/core/VisualizationBuilder.scala
@@ -8,10 +8,11 @@ import pythia.config.PythiaConfig
 
 class VisualizationBuilder {
 
-  val visualizationHost = InetAddress.getLocalHost.getHostName
+  val visualizationHost = PythiaConfig.HOSTNAME
   val visualizationPort = PythiaConfig.WEB_PORT
 
   def buildVisualizations(clusterId: String, context: StreamingContext, configuration: PipelineConfiguration, outputStreams: Map[(String, String), DStream[Instance]]): Unit = {
+    // Memory viz is created by spark using spark metrics!
     // Global viz
     val visualizationClient = new VisualizationClient(visualizationHost, visualizationPort, clusterId, "pipeline-latency")
     val baseVisualization = new LatencyVisualization()
---
+++ src/main/scala/pythia/visualization/JvmVisualizationSink.scala
@@ -0,0 +1,67 @@
+package org.apache.spark.metrics.sink
+
+import java.util.Properties
+import com.codahale.metrics._
+import java.util.concurrent.TimeUnit
+import java.util
+import pythia.core.VisualizationClient
+import pythia.config.PythiaConfig
+import org.apache.spark.SecurityManager
+
+class JvmVisualizationSink(val property: Properties, val registry: MetricRegistry, securityMgr: SecurityManager) extends Sink {
+
+  val clusterId = "local"
+  val reporter = new JvmVisualizationReporter(clusterId, "pipeline-memory", registry)
+
+  override def start(): Unit = {
+    reporter.start(10, TimeUnit.SECONDS)
+  }
+
+  override def stop(): Unit = {
+    reporter.stop()
+  }
+
+  override def report(): Unit = {
+    reporter.report()
+  }
+}
+
+class JvmVisualizationReporter (clusterId: String, visualizationId: String, registry: MetricRegistry,
+    name: String = "visualization-reporter",
+    filter: MetricFilter = MetricFilter.ALL,
+    rateUnit: TimeUnit = TimeUnit.SECONDS,
+    durationUnit: TimeUnit = TimeUnit.MILLISECONDS)
+  extends ScheduledReporter(
+    registry: MetricRegistry,
+    name: String, filter: MetricFilter,
+    rateUnit: TimeUnit,
+    durationUnit: TimeUnit) {
+
+  val visualizationClient = new VisualizationClient(PythiaConfig.HOSTNAME, PythiaConfig.WEB_PORT, clusterId, visualizationId)
+
+  override def report(
+    gauges: util.SortedMap[String, Gauge[_]],
+    counters: util.SortedMap[String, Counter],
+    histograms: util.SortedMap[String, Histogram],
+    meters: util.SortedMap[String, Meter],
+    timers: util.SortedMap[String, Timer]): Unit = {
+
+    val memoryUsed = Option(gauges.get("jvm.total.used")) match {
+      case Some(gauge) => gauge.getValue.toString.toDouble
+      case None => 0.0
+    }
+
+    val memoryCommitted = Option(gauges.get("jvm.total.committed")) match {
+      case Some(gauge) => gauge.getValue.toString.toDouble
+      case None => 0.0
+    }
+
+    val maxMemory = Option(gauges.get("jvm.total.max")) match {
+      case Some(gauge) => gauge.getValue.toString.toDouble
+      case None => 0.0
+    }
+
+    val data = Map ("memory.used" -> memoryUsed, "memory.max" -> maxMemory, "memory.committed" -> memoryCommitted)
+    visualizationClient.send(System.currentTimeMillis, data)
+  }
+}
